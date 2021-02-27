fa0f0feea7b58165d30c7a1e361abef18f3d679f
Kristián Valčo <72340101+kristianvalco@users.noreply.github.com>
2021-02-27 18:20:15 +0100

    working auth system

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import menuIcon from '../../assets/img/icons/menu.svg'
 import app from '../../base';
+
+import avatar from '../../assets/img/avatar.jpg'
 
 // CSS
 import './Navigation.scss'
@@ -11,11 +12,24 @@ const Navigation = (props) => {
     const { handle } = props
 
     return (
-        <nav className="navbar navbar-expand-lg">
-            <div className="container-fluid">
-                <button onClick={handle} type="button" className="close-btn"><img src={menuIcon} alt="" /></button>
-                <button onClick={() => app.auth().signOut()} type="button" className="btn btn-primary me-auto shadow-none" >Sign out</button>
-            </div>
+        <nav className="navbar navbar-expand">
+            <button onClick={handle} type="button" className="close-btn">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 15"><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path className="menuIcon" d="M0,3V0H20V3ZM0,9V6H16V9Zm0,6V12H20v3Z" /></g></g></svg>
+            </button>
+            <ul className="navbar-nav">
+                <li className="nav-item dropdown">
+                    <button className="nav-link dropdown-toggle avatar" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src={avatar} alt="" />Kristián Valčo
+                    </button>
+                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li>
+                            <button className="dropdown-item" onClick={() => app.auth().signOut()}>Odhlásiť sa</button>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+
+            {/* <button onClick={() => app.auth().signOut()} type="button" className="btn btn-primary shadow-none" >Sign out</button> */}
         </nav>
     )
 }
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link, NavLink } from "react-router-dom";
 import logo from '../../assets/img/sosthe-logo.svg'
-import plusIcon from "../../assets/img/icons/plus.svg";
 
 // CSS
 import './Sidebar.scss'
@@ -16,13 +15,22 @@ const Sidebar = (props) => {
             <div className="sidebar-header">
                 <Link to="/" className="sidebarLogo"><img src={logo} alt="" /></Link>
             </div>
-
-            <button className="btn btn-primary addNew shadow-none">Pridať záznam <img src={plusIcon} alt="" /></button>
-
+            <button className="btn btn-primary addNew shadow-none">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 97 97"><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path className="plusIcon" d="M89.54,41H56V7.46A7.46,7.46,0,0,0,41,7.46V41H7.46A7.46,7.46,0,0,0,7.46,56H41V89.54a7.46,7.46,0,0,0,14.92,0V56H89.54a7.46,7.46,0,0,0,0-14.92Z"/></g></g></svg>
+                Pridať záznam
+            </button>
             <ul className="side-navbar">
                 <li className="side-navbar-item">
-                    <NavLink to="/" exact>Domov</NavLink>
-                    <NavLink to="/contact">Contact</NavLink>
+                    <NavLink to="/" exact>
+                        <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 215 229.13"><path d="M115,2.71a11.8,11.8,0,0,0-15.06,0L7.53,83.55C3.39,87.17,0,94.63,0,100.13v119a10,10,0,0,0,10,10H69a10,10,0,0,0,10-10v-71a10,10,0,0,1,10-10h37a10,10,0,0,1,10,10v71a10,10,0,0,0,10,10h59a10,10,0,0,0,10-10v-119c0-5.5-3.39-13-7.53-16.58Z" transform="translate(0 0)"/></svg>
+                        Domov
+                    </NavLink>
+                </li>
+                <li className="side-navbar-item">
+                    <NavLink to="/contact">
+                        <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 215 229.13"><path d="M115,2.71a11.8,11.8,0,0,0-15.06,0L7.53,83.55C3.39,87.17,0,94.63,0,100.13v119a10,10,0,0,0,10,10H69a10,10,0,0,0,10-10v-71a10,10,0,0,1,10-10h37a10,10,0,0,1,10,10v71a10,10,0,0,0,10,10h59a10,10,0,0,0,10-10v-119c0-5.5-3.39-13-7.53-16.58Z" transform="translate(0 0)"/></svg>
+                        Contact
+                    </NavLink>
                 </li>
             </ul>
         </nav>
