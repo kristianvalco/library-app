77180f9584096016998f1e18087ea9c9467557e3
Kristián Valčo <72340101+kristianvalco@users.noreply.github.com>
2021-03-01 17:55:51 +0100

    work app

--- server/index.js
+++ server/index.js
@@ -14,6 +14,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+app.use('/', (req, res) => {
+    res.send('Hello to library-app API');
+});
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
