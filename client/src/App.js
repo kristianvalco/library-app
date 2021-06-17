d5f01f08d1613470041a460c3ec096b0f892eca4
Kristián Valčo <kristian.valco@gmail.com>
2021-06-17 12:52:05 +0200

    app works

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 // CSS
 import './assets/css/App.scss'
